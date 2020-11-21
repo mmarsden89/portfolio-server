62c453408a1f8349efd7ca12b4269739e476db2c
mmarsden89 <mattmarsdendev@gmail.com>
2020-11-20 17:22:59 -0800

    set up server

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
+require("dotenv").config();
 const express = require("express");
+const http = require("http");
+const cors = require("cors");
 
 // create new express app and save it as "app"
 const app = express();
 
 // server configuration
-const PORT = 8080;
+const serverDevPort = 8080;
+const clientDevPort = 3000;
 
-// create a route for the app
+// setting cors origin
+app.use(
+  cors({
+    origin: process.env.CLIENT_ORIGIN || `http://localhost:${clientDevPort}`,
+  })
+);
+
+// set port to env for production or development
+const PORT = process.env.PORT || serverDevPort;
+
+// create a route for the app which gets the weather data
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  const options = {
+    host: "api.openweathermap.org",
+    path: `/data/2.5/weather?q=Seattle&units=imperial&appid=${process.env.WEATHER_API}`,
+    method: "GET",
+  };
+
+  const request = http.request(options, function (response) {
+    const body = "";
+    response.on("data", function (data) {
+      body += data;
+    });
+    response.on("end", function () {
+      res.send(JSON.parse(body));
+    });
+  });
+  request.on("error", function (e) {
+    console.log("Problem with request: " + e.message);
+  });
+  request.end();
 });
 
 // make the server listen to requests
